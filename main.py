dcf74ffb388962e4cd3f3570535d7c6084a91599
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2023-02-20 22:47:10 +0200

    added arguments

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+"""
+"""
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('year')
+parser.add_argument('latitude')
+parser.add_argument('longtitude')
+parser.add_argument('dataset')
+parser.add_argument('film')
 
+args = parser.parse_args()
+
+year = args.year
+latitude = args.latitude
+longtitude = args.longtitude
+dataset = args.dataset
+film = args.film
